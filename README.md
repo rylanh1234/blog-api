0d2ed191b563c11b054073602b8c11ca15ab7904
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2026-03-05 17:46:11 -0700

    Set up Express, Prisma, part of the REST API and front-end

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # blog-api
+REST API blog with two front-ends.
+One for reading and commenting on posts, the other to write and publish the posts
---
+++ controllers/usersController.js
@@ -0,0 +1,5 @@
+exports.usersGet = async (req, res) => {
+    const prisma = require("../app");
+    const users = await prisma.user.findMany();
+    res.json(users);
+}

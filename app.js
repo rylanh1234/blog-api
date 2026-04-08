e61585bd86d26762bdff613b72f7acdf8b100e10
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2026-04-08 16:45:27 -0600

    Added logins Added password to User model

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const usersRouter = require("./routes/usersRouter");
 require("dotenv/config");
 const { PrismaPg } = require('@prisma/adapter-pg');  // For other db adapters, see Prisma docs
 const { PrismaClient } = require('./generated/prisma/client');
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const jwt = require("jsonwebtoken");
+app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
@@ -14,6 +18,54 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 module.exports = prisma;
+
+// configure JWT Strategy
+const opts = {
+  jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "secret_key"
+};
+
+passport.use(
+  new passportJWT.Strategy(opts, async (payload, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: payload.userId }
+      });
+      if (user) return done(null, user);
+      return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+// login route
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email: username } });
+  if (!user || user.password !== password) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    { userId: user.id },
+    "secret_key",
+    { expiresIn: "1h" }
+  );
+  res.json({ token });
+})
+
+// protect routes
+app.get(
+  "/api/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      message: "Protected route",
+      user: req.user
+    });
+  }
+);
 
 app.use("/api", usersRouter);
 app.use("/", pageRouter);
